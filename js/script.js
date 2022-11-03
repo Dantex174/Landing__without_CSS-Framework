dab06c56abe3db8ced1640e37e1807bfabbf5272
Grigory Saifullin <grigory.saifullin@yandex.ru>
2022-11-03 22:03:21 +0600

    Created Menu section with tabs and added mobile adaptability

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $('.header__burger').click(function(event) {
-        $('.header__burger, .menu').toggleClass('active');
+        $('.header__burger, .navMenu').toggleClass('active');
         $('body').toggleClass('lock');
+    });
+    $('.menu__food, .menu__drinks').click(function(event) {
+        $('.menu__food, .menu__drinks, .menu__food-cards, .menu__drinks-cards').toggleClass('selected');
     });
 });
