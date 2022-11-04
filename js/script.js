d9ba5286f63f0c43d083576ce9caa4e3b10838d8
Grigory Saifullin <grigory.saifullin@yandex.ru>
2022-11-04 17:01:29 +0600

    Created section Testimonials

--- js/script.js
+++ js/script.js
@@ -6,4 +6,16 @@ $(document).ready(function(){
     $('.menu__food, .menu__drinks').click(function(event) {
         $('.menu__food, .menu__drinks, .menu__food-cards, .menu__drinks-cards').toggleClass('selected');
     });
+    $('#selector_one').click(function(event) {
+        $('#testimonial_one, #selector_one').addClass('active');
+        $('#testimonial_two, #testimonial_three, #selector_two, #selector_three').removeClass('active');
+    });
+    $('#selector_two').click(function(event) {
+        $('#testimonial_two, #selector_two').addClass('active');
+        $('#testimonial_one, #testimonial_three, #selector_one, #selector_three').removeClass('active');
+    });
+    $('#selector_three').click(function(event) {
+        $('#testimonial_three, #selector_three').addClass('active');
+        $('#testimonial_one, #testimonial_two, #selector_one, #selector_two').removeClass('active');
+    });
 });
