06550420786f84a9b10832029e97fab39c773fb8
Grigory Saifullin <grigory.saifullin@yandex.ru>
2022-11-05 12:07:46 +0600

    Fixed header__burger

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
         $('.header__burger, .navMenu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    $('.navMenu__link').click(function(event) {
+        $('.header__burger, .navMenu').removeClass('active');
+        $('body').removeClass('lock');
+    })
     $('.menu__food, .menu__drinks').click(function(event) {
         $('.menu__food, .menu__drinks, .menu__food-cards, .menu__drinks-cards').toggleClass('selected');
     });
